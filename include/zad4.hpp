157112e016a1a581e62907ba624b682afb20c8c9
michaljasak <01123865@pw.edu.pl>
2021-01-15 08:38:24 +0000

    asdasdadw11sad

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,4 +4,11 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
-std::vector 
+std::vector<Artysta> stworzZespol(int l, const std::string& nazwa)
+{
+  std::vector<Artysta> w;
+  if(nazwa.size() != l) throw std::logic_error("not this time");
+
+  for(int i=0; i< l+1; i++) {w.push_back(Artysta(nazwa,static_cast<unsigned>(i)));}
+  return wektor;
+}
