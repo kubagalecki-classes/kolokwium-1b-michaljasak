115119bbb3164e259263d1fcdfb65a783e94c9b5
michaljasak <01123865@pw.edu.pl>
2021-01-15 08:41:36 +0000

    aas

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -12,15 +12,16 @@ using namespace std;
 
 class Artysta 
 {
-	string pseudo;
-	unsigned sluch;
+	
 public:
-	Artysta() { pseudo = "Anonim"; sluch = 0; }
+	Artysta() : pseudo("Anonim"), sluch(0) { }
 	Artysta(const std::string& a, unsigned b) : pseudo(a), sluch(b) {}
 	~Artysta() {};
 	virtual void graj(std::ostream& a) const {a<<pseudo << ": " << sluch;	};
-	const std::string& getPseudonim() const
-	{return pseudo;}
-	unsigned getSluchacze() const
-	{return sluch;}
+	const std::string& getPseudonim() const {return pseudo;}
+	unsigned getSluchacze() const {return sluch;}
+
+  private:
+    std::string pseudo;
+	  unsigned sluch;
 };
